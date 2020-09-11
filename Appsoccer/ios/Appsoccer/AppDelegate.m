6fdce0ce60e1c5d853d93d25ee7fcd2d46250c96
Ariel Gonzalez <arielgonzalez@MacBook-Pro.local>
2020-09-10 23:05:09 -0500

    Login, logout and SignUP. Android and IOS

--- Appsoccer/ios/Appsoccer/AppDelegate.m
+++ Appsoccer/ios/Appsoccer/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <Firebase.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -26,7 +27,12 @@ static void InitializeFlipper(UIApplication *application) {
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+
 {
+  
+  if ([FIRApp defaultApp] == nil) {
+       [FIRApp configure];
+  }
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
